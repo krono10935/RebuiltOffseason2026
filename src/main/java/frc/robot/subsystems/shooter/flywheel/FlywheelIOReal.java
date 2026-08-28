569d23dcb0ecf99490ad5757e1454bddcb399255
dandush <dandabonu@gmail.com>
2026-08-28 16:07:05 +0300

    Actually used ctrl C and uploaded the code from last commit

--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIOReal.java
@@ -1,5 +1,60 @@
 package frc.robot.subsystems.shooter.flywheel;
 
-public class FlywheelIOReal {
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
+import frc.lib.math.IsNear;
+
+public class FlywheelIOReal implements FlywheelIO {
+
+    private final TalonFX[] flywheelMotors;
+
+    public FlywheelIOReal()
+    {
+        flywheelMotors = new TalonFX[4];
+
+        flywheelMotors[0] = new TalonFX(FlywheelConstants.MOTOR_IDS[0]); // flywheelMotors[0] is the leading motor
+        flywheelMotors[0].getConfigurator().apply(FlywheelConstants.getLeadConfig(FlywheelConstants.LEAD_INVERTED));
+
+        for (int i = 1; i < flywheelMotors.length; i++){
+            flywheelMotors[i] = new TalonFX(FlywheelConstants.MOTOR_IDS[i]);
+
+            flywheelMotors[i].getConfigurator().apply(FlywheelConstants.getFollowerConfig(i >= flywheelMotors.length / 2)); // set half the motors to inverted
+
+            flywheelMotors[i].setControl(new Follower(FlywheelConstants.MOTOR_IDS[0], MotorAlignmentValue.Aligned)); // TODO: understand how aligned works, we set it to inverted above this
+        }
+    }
+
+    /**
+     * @return returns the lead motor
+     */
+    private TalonFX getLeadMotor()
+    {
+        return flywheelMotors[0];
+    }
+
+    @Override
+    public void setSpeed(double mps) {
+        getLeadMotor().setControl(new VelocityVoltage(mps).withSlot(0));
+    }
+
+    @Override
+    public void holdSpeed(double mps) {
+        getLeadMotor().setControl(new VelocityVoltage(mps).withSlot(1));
+    }
+
+    @Override
+    public void stop() {
+        getLeadMotor().stopMotor();
+    }
+
+    @Override
+    public void updateInputs(FlywheelInputs inputs) {
+        inputs.speedMPS = getLeadMotor().getVelocity().getValueAsDouble();
+
+        inputs.isAtGoal = IsNear.isNear(inputs.speedMPS, getLeadMotor().getClosedLoopReference().getValueAsDouble(), FlywheelConstants.MPS_TOLERANCE);
+    }
 
 }
--- src/main/java/frc/robot/subsystems/shooter/hood/HoodIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodIOReal.java
@@ -1,5 +1,45 @@
 package frc.robot.subsystems.shooter.hood;
 
-public class HoodIOReal {
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.lib.math.IsNear;
+
+public class HoodIOReal implements HoodIO{
+
+    private final SparkMax hoodMotor;
+
+    public HoodIOReal()
+    {
+        hoodMotor = new SparkMax(HoodConstants.HOOD_MOTOR_CANID, MotorType.kBrushless);
+        hoodMotor.configure(HoodConstants.getHoodConfig(), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void setAngle(Rotation2d angle) {
+        hoodMotor.getClosedLoopController().setSetpoint(angle.getRotations(), ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    }
+
+    @Override
+    public void holdAngle(Rotation2d angle) {
+        hoodMotor.getClosedLoopController().setSetpoint(angle.getRotations(), ControlType.kPosition, ClosedLoopSlot.kSlot1);
+    }
+
+    @Override
+    public void stop() {
+        hoodMotor.stopMotor();
+    }
+
+    @Override
+    public void updateInputs(HoodInputs inputs) {
+        inputs.currentAngle = Rotation2d.fromRotations(hoodMotor.getEncoder().getPosition());
+
+        inputs.isAtGoal = IsNear.isNear(inputs.currentAngle, Rotation2d.fromRotations(hoodMotor.getClosedLoopController().getSetpoint()), HoodConstants.DEGREE_TOLERANCE);
+    }
 
 }
