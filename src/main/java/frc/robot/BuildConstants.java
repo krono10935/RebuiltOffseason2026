2859ca1b782070a455182e9dcc63a98cbd4997b2
ItayDeMagniv <itaypeleg126@gmail.com>
2026-08-30 17:21:34 +0300

    Added the pivot subsystem
    The pivot subsystem was added
    -Aviv Shaked

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RebuiltOffseason2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "be8505aa95896ad0037be56876cfb79f147b2c54";
-  public static final String GIT_DATE = "19-08-2026 23:04:44 GMT+02:00";
-  public static final String GIT_BRANCH = "autonomous";
-  public static final String BUILD_DATE = "20-08-2026 16:47:49 GMT+02:00";
-  public static final long BUILD_UNIX_TIME = 1787237269842L;
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "ccfd9dc850dc649fc17940c813ec0b7471ff3a1c";
+  public static final String GIT_DATE = "24-08-2026 19:32:07 GMT+02:00";
+  public static final String GIT_BRANCH = "intakeSubsystem";
+  public static final String BUILD_DATE = "30-08-2026 16:08:15 GMT+02:00";
+  public static final long BUILD_UNIX_TIME = 1788098895356L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotSubsystem.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems.intake.pivot;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PivotSubsystem extends SubsystemBase {
+    private final PivotInputsAutoLogged inputs;
+    private final PivotIO io;
+
+    //Create a new PivotSubsystem
+    public PivotSubsystem(){
+        inputs = new PivotInputsAutoLogged();
+        io = new PivotIOCTRE();
+    }
+
+    @Override
+    public void periodic(){
+        updateInputs();
+        Logger.processInputs(getName(), inputs);
+    }
+
+    /**
+     * Get the motor temprature
+     * @return The temprature of the motor in celsius 
+     */
+    public double getMotorTempC(){
+        return inputs.motorTempC;
+    }
+
+    /**
+     * Get the pivot angle
+     * @return The angle of the pivot
+     */
+    public Rotation2d getangle(){
+        return inputs.angle;
+    }
+
+    /**
+     * Get the angular velocity of the roller
+     * @return The angular velocity of the pivot in RPS
+     */
+    public double getAngularVelocityRPS(){
+        return inputs.angularVelocityRPS;
+    }
+
+    /**
+     * Command the hardware to go to a rotation
+     * @param rotation the wanted rotation
+     */
+    public void setRotation(Rotation2d rotation){
+        io.setRotation(rotation);
+    }
+
+    /**
+     * Stops the pivot
+     */
+    public void stop(){
+        io.stop();
+    }
+
+    /**
+     * Updates the field values of the input object.
+     * @param inputs The input object that we are updating.
+     */
+    private void updateInputs(){
+        io.updateInputs(inputs);
+    }
+}
