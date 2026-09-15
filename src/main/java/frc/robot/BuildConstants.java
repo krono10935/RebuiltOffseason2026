06a9036338f6279abeae62c8dd8a759eed7058c6
dandush <dandabonu@gmail.com>
2026-09-15 21:23:48 +0300

    Added ShotCalculator from season
    fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun fun

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RebuiltOffseason2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "859b13bc5deb201b705e71613d7bb11c14f5c67d";
-  public static final String GIT_DATE = "13-09-2026 10:34:37 GMT+02:00";
-  public static final String GIT_BRANCH = "shooter";
-  public static final String BUILD_DATE = "15-09-2026 15:27:16 GMT+02:00";
-  public static final long BUILD_UNIX_TIME = 1789478836941L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "28a23d1297a1e597c5f1951038769d795024165d";
+  public static final String GIT_DATE = "15-09-2026 17:39:51 GMT+02:00";
+  public static final String GIT_BRANCH = "shooterCommands";
+  public static final String BUILD_DATE = "15-09-2026 20:20:16 GMT+02:00";
+  public static final long BUILD_UNIX_TIME = 1789496416523L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
